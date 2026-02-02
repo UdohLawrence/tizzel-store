0d123e361a50d42b907ff47316dd6ae553abf6a2
UdohLawrence <lawrenceaudoh@gmail.com>
2026-02-02 21:03:55 +0100

    refactor index and about pages

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,5 +4,24 @@ import { Controller, Get, Render } from '@nestjs/common';
 export class AppController {
   @Get('/')
   @Render('index')
-  index() {}
+  index() {
+    return {
+      title: 'Home Page - Online Store',
+    };
+  };
+
+  @Get('/about')
+  @Render('about')
+  about() {
+    const viewData = [];
+
+    viewData['description'] = 'This is the about page';
+    viewData['author'] = 'Developed by Ekomobong Lawrence';
+    viewData['title'] = 'About Us - Tizzel Store';
+    viewData['subtitle'] = 'About Us';
+
+    return {
+      viewData: viewData
+    };
+  }
 }
